7db2f6a2f61f5207d947d03bad94822e42bc43a1
SUMISTHA ROY <sumistharoy45@gmail.com>
2026-02-20 11:31:47 +0530

    Tailwind CSS integrated, site working

--- sumistha/src/main.jsx
+++ sumistha/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+<h1 className="text-red-500 text-2xl">Tailwind Test</h1>
